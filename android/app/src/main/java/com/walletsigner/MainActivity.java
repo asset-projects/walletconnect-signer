b8cdee1ef45b5dcd14194563d0c89ecfbe5536e0
YutaSugimura <nextc3m@gmail.com>
2022-02-21 20:22:20 +0900

    fix

--- android/app/src/main/java/com/walletsigner/MainActivity.java
+++ android/app/src/main/java/com/walletsigner/MainActivity.java
@@ -1,4 +1,4 @@
-package com.wallet;
+package com.walletsigner;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
--- android/app/src/main/java/com/walletsigner/MainApplication.java
+++ android/app/src/main/java/com/walletsigner/MainApplication.java
@@ -1,4 +1,4 @@
-package com.wallet;
+package com.walletsigner;
 
 import androidx.multidex.MultiDexApplication;
 import android.app.Application;
@@ -63,7 +63,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.wallet.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.walletsigner.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
