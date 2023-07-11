ae4b581844b0108c727095e80da41c289e3419aa
YutaSugimura <nextc3m@gmail.com>
2023-07-11 21:57:29 +0900

    feat: add react-native-quick-crypto

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          crypto: 'react-native-quick-crypto',
+          stream: 'stream-browserify',
+          buffer: '@craftzdog/react-native-buffer',
+        },
+      },
+    ],
+  ],
 };
