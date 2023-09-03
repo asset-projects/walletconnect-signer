2cd5f34529c476496f13a6fcc9f4e2519a248502
YutaSugimura <nextc3m@gmail.com>
2023-09-03 16:43:50 +0900

    fix: update qrcodeScanner

--- src/features/qrcodeScanner/index.tsx
+++ src/features/qrcodeScanner/index.tsx
@@ -1,17 +1,29 @@
 import React, {type FC} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {COLORS} from '../../commons';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
 
 type Props = {};
 
 export const QRCodeScanner: FC<Props> = () => {
+  const devices = useCameraDevices('wide-angle-camera');
+  const device = devices.back;
+
+  if (device == null) {
+    return (
+      <View style={styles.cameraViewContainer}>
+        <Text style={styles.text}>
+          QRCodeScanner is currently under preparation.
+        </Text>
+
+        <Text style={styles.text}>Please enter uri from form</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.cameraViewContainer}>
-      <Text style={styles.text}>
-        QRCodeScanner is currently under preparation.
-      </Text>
-
-      <Text style={styles.text}>Please enter uri from form</Text>
+      <Camera style={StyleSheet.absoluteFill} device={device} isActive />
     </View>
   );
 };
