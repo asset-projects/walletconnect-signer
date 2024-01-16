d606902d16f468076b8c5d40e0818cfc034c5644
YutaSugimura <nextc3m@gmail.com>
2024-01-16 21:10:38 +0900

    style: remove unused code and simplify StatusBar barStyle

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,16 +1,14 @@
 import React, {type FC} from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import {COMMON_STYLES} from '../commons/styles';
 import {HomeHeader} from '../components/homeHeader';
 import {WalletCard} from '../features/wallet';
 import {WalletConnectSessions} from '../features/walletconnect';
 
 const Screen: FC = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaView style={[COMMON_STYLES.flex1, COMMON_STYLES.backgroundColor]}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar />
 
       <HomeHeader />
       <WalletCard />
